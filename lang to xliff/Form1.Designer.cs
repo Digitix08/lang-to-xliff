cf9fafdc5f3de410f0e3f82daf33852128c02c3d
Gabi Dima <dg.ajtx@gmail.com>
2025-06-24 14:27:30 +0300

    Add project files.

---
+++ lang to xliff/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace format_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.currentModeLangToXliffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.currentModeLangToXliffToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFilesToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFilesToolStripMenuItem
+            // 
+            this.openFilesToolStripMenuItem.Name = "openFilesToolStripMenuItem";
+            this.openFilesToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.openFilesToolStripMenuItem.Text = "Open files";
+            this.openFilesToolStripMenuItem.Click += new System.EventHandler(this.openFilesToolStripMenuItem_Click);
+            // 
+            // currentModeLangToXliffToolStripMenuItem
+            // 
+            this.currentModeLangToXliffToolStripMenuItem.Name = "currentModeLangToXliffToolStripMenuItem";
+            this.currentModeLangToXliffToolStripMenuItem.Size = new System.Drawing.Size(159, 20);
+            this.currentModeLangToXliffToolStripMenuItem.Text = "Current mode: lang to xliff";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(200, 16);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output:";
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(49, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(651, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "No file selected";
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(49, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(651, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "No file selected";
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 3);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(794, 358);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "log output...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(706, 3);
+            this.button1.Name = "button1";
+            this.tableLayoutPanel1.SetRowSpan(this.button1, 2);
+            this.button1.Size = new System.Drawing.Size(88, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 46F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 97F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 404);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem currentModeLangToXliffToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ lang to xliff/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace format_converter
+{
+    public partial class Form1 : Form
+    {
+        string file1 = "", file2 = "", saveloc;
+        int strcount1 = 0, strcount2 = 0, trcount2 = 0, utrcount2 = 0;
+        XmlDocument xliff = new XmlDocument();
+        BackgroundWorker backgroundWorker1 = new BackgroundWorker();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (file1 != "" && file2 != "") {
+                button1.Text = "Cancel";
+                openFilesToolStripMenuItem.Enabled = false;
+                button1.Click += BTN_Cancel;
+                if(!backgroundWorker1.IsBusy)
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else if (MessageBox.Show("No files selected. Select now?", "No file selected", MessageBoxButtons.YesNo) == DialogResult.Yes) LoadFiles();
+        }
+
+        private void BTN_Cancel(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Operation is running. Do you want to cancel it? Note: you will lose all progress", "Cancel operation?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                backgroundWorker1.CancelAsync();
+                button1.Text = "Convert";
+                openFilesToolStripMenuItem.Enabled = false;
+                button1.Click += button1_Click;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBackgroundWorker();
+        }
+
+        void InitializeBackgroundWorker()
+        {
+            backgroundWorker1.DoWork +=
+                 backgroundWorker1_DoWork;
+            backgroundWorker1.RunWorkerCompleted +=
+                backgroundWorker1_RunWorkerCompleted;
+            backgroundWorker1.ProgressChanged +=
+                backgroundWorker1_ProgressChanged;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        void cancelAsyncButton_Click(object sender,
+        EventArgs e)
+        {
+            // Cancel the asynchronous operation.
+            backgroundWorker1.CancelAsync();
+
+            // Disable the Cancel button.
+            //cancelAsyncButton.Enabled = false;
+        }
+
+        void backgroundWorker1_DoWork(object sender,
+        DoWorkEventArgs e)
+        {
+            // Get the BackgroundWorker that raised this event.
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            // Assign the result of the computation
+            // to the Result property of the DoWorkEventArgs
+            // object. This is will be available to the 
+            // RunWorkerCompleted eventhandler.
+            e.Result = ConvertFiles(worker, e);
+        }
+
+        void backgroundWorker1_RunWorkerCompleted(
+        object sender, RunWorkerCompletedEventArgs e)
+        {
+            // First, handle the case where an exception was thrown.
+            if (e.Error != null)
+            {
+                _ = MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                // Next, handle the case where the user canceled 
+                // the operation.
+                // Note that due to a race condition in 
+                // the DoWork event handler, the Cancelled
+                // flag may not have been set, even though
+                // CancelAsync was called.
+                toolStripProgressBar1.Value = 0;
+                toolStripStatusLabel1.Text = "Canceled";
+            }
+            else
+            {
+                // Finally, handle the case where the operation 
+                // succeeded.
+                MessageBox.Show("done");
+            }
+
+            // Enable the UpDown control.
+            fileToolStripMenuItem.Enabled = true;
+        }
+
+        void backgroundWorker1_ProgressChanged(object sender,
+        ProgressChangedEventArgs e)
+        {
+            int prog = (int)((float)e.ProgressPercentage / strcount1 * 100);
+            int val2 = strcount1;
+            if (prog > 100 && strcount2 > strcount1) { prog = (int)((float)e.ProgressPercentage / strcount2 * 100); val2 = strcount2; }
+            toolStripProgressBar1.Value = prog;
+            toolStripStatusLabel1.Text = e.ProgressPercentage.ToString() + " / " + val2.ToString();
+        }
+
+        private void LoadFiles()
+        {
+            //file1 = ""; file2 = "";
+            strcount1 = 0; strcount2 = 0; trcount2 = 0; utrcount2 = 0;
+            string line1 = "";
+            toolStripStatusLabel1.Text = "Select Input...";
+            OpenFileDialog fdiag = new OpenFileDialog
+            {
+                Title = "Open input file...",
+                Filter = "Lang files (*.lang)|*.lang",
+                InitialDirectory = "%homedir%",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            StreamReader S;
+            StringBuilder P;
+            if (fdiag.ShowDialog() == DialogResult.OK)
+            {
+                toolStripStatusLabel1.Text = "Loading file 1...";
+                toolStripProgressBar1.Style = ProgressBarStyle.Marquee;
+                toolStripProgressBar1.Value = 100;
+                S = new StreamReader(fdiag.FileName);
+                P = new StringBuilder(file1);
+                line1 = S.ReadLine();
+                while (line1 != null)
+                {
+                    P.AppendLine(line1);
+                    if (line1.Length > 0) if (line1[0] != '#') strcount1++;
+                    line1 = S.ReadLine();
+                }
+                file1 = P.ToString();
+                toolStripProgressBar1.Style = ProgressBarStyle.Blocks;
+                toolStripProgressBar1.Value = 50;
+                label3.Text = fdiag.FileName;
+                if(textBox1.Text == "log output...")textBox1.Clear();
+                textBox1.AppendText(fdiag.FileName + " has " + Convert.ToString(strcount1) + " strings" + Environment.NewLine);
+                toolStripStatusLabel1.Text = "Loading file 2...";
+                fdiag.Title = "Open output file...";
+                fdiag.Filter = "Lang files (*.xliff)|*.xliff";
+                if (fdiag.ShowDialog() == DialogResult.OK)
+                {
+                    toolStripProgressBar1.Style = ProgressBarStyle.Marquee;
+                    toolStripProgressBar1.Value = 100;
+                    saveloc = fdiag.FileName;
+                    S = new StreamReader(fdiag.FileName);
+                    P = new StringBuilder(file2);
+                    line1 = S.ReadLine();
+                    while (line1 != null)
+                    {
+                        P.AppendLine(line1);
+                        if (line1.IndexOf("resname") != -1) strcount2++;
+                        else if (line1.IndexOf("state=\"final\"") != -1 || line1.IndexOf("state=\"translated\"") != -1 || line1.IndexOf("state=\"needs - review - translation\"") != -1) trcount2++;
+                        else if (line1.IndexOf("needs-translation") != -1) utrcount2++;
+                        line1 = S.ReadLine();
+                    }
+                    xliff.Load(fdiag.FileName);
+                    file2 = P.ToString();
+                    label4.Text = fdiag.FileName;
+                    toolStripProgressBar1.Style = ProgressBarStyle.Blocks;
+                    textBox1.AppendText(fdiag.FileName + " has " + Convert.ToString(strcount2) + " strings of whom " + Convert.ToString(trcount2) + " translated and " + Convert.ToString(utrcount2) + " untranslated" + Environment.NewLine);
+                }
+                else MessageBox.Show("File not selected!", "Warning");
+                toolStripStatusLabel1.Text = "Ready";
+                textBox1.AppendText("Input length: " + Convert.ToString(file1.Length) + Environment.NewLine);
+                textBox1.AppendText("Output length: " + Convert.ToString(file2.Length) + Environment.NewLine);
+            }
+            else MessageBox.Show("File not selected!", "Warning");
+        }
+        private void openFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadFiles(); 
+        }
+
+        private int ConvertFiles(BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            string line1, line11, line12;
+            int loc1 = 0, tloc = 0, tms = 0;
+            long loc2 = 0;
+            StringReader fconvert1 = new StringReader(file1);
+            XmlNode newTransl, transl;
+            XmlNodeList nodes = xliff.GetElementsByTagName("trans-unit"), nodes2;
+            nodes2 = nodes;
+            //textBox1.AppendText(nodes.Count.ToString() + " entries");
+            if (MessageBox.Show("Are you sure you want to convert?", "ready to convert", MessageBoxButtons.YesNo) == DialogResult.Yes) {
+                line1 = fconvert1.ReadLine();
+                while (line1 != null)
+                {
+                    if (worker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                    }
+                    else if (line1.Length > 0) if (line1[0] != '#')
+                        {
+                            line11 = line1.Substring(line1.IndexOf("=")+1);
+                            line1 = line1.Substring(0, line1.IndexOf("="));
+                            loc1++;
+                            if (line11.IndexOf('&') != -1) { line11 = line11.Substring(0, line11.IndexOf('&')); line12 = line11.Substring(line11.IndexOf('&') + 1); line11 = line11 + "&amp;" + line12; }
+                            if (line11.IndexOf('<') != -1) { line11 = line11.Substring(0, line11.IndexOf('<')); line12 = line11.Substring(line11.IndexOf('&') + 1); line11 = line11 + "&lt;" + line12; }
+                            if (line11.IndexOf('>') != -1) { line11 = line11.Substring(0, line11.IndexOf('>')); line12 = line11.Substring(line11.IndexOf('&') + 1); line11 = line11 + "&gt;" + line12; }
+                            if (line11.IndexOf('\'') != -1) { line11 = line11.Substring(0, line11.IndexOf('\'')); line12 = line11.Substring(line11.IndexOf('&') + 1); line11 = line11 + "&apos;" + line12; }
+                            if (line11.IndexOf('"') != -1) { line11 = line11.Substring(0, line11.IndexOf('"')); line12 = line11.Substring(line11.IndexOf('&') + 1); line11 = line11 + "&quot;" + line12; }
+                            for (int i = 0; i < Convert.ToInt64(nodes.Count.ToString()); i++)
+                                if (nodes[i].Attributes["resname"].Value == line1){ 
+                                    loc2++;
+                                    if(tloc == 0)foreach(XmlNode nb in nodes[i])
+                                        {
+                                            if(nb.Name == "target")tloc = tms;
+                                            tms++;
+                                        }
+                                    transl = nodes[i].ChildNodes.Item(tloc);
+                                    if (transl != null) {
+                                        newTransl = transl;
+                                        newTransl.InnerXml = line11;
+                                        newTransl.Attributes["state"].Value = "translated";
+                                        //MessageBox.Show("replaced " + transl.InnerXml + " with " + newTransl.InnerXml);
+                                        nodes[i].ReplaceChild(transl, newTransl);
+                                    }
+                                    //if (newTransl.Attributes["state"].Value == "needs-translation") MessageBox.Show(line11, "we need this here");
+                                    worker.ReportProgress(loc1);
+                                    break;
+                                }
+                        }
+                    line1 = fconvert1.ReadLine();
+                }
+                int tll = 0;
+                XmlNode Child0 = xliff.GetElementsByTagName("xliff")[0];
+                foreach (XmlNode a in Child0)
+                {
+                    if (a.Name == "file")
+                        break;
+                    tll++;
+                }
+                XmlNode Child1 = Child0.ChildNodes.Item(tll);
+                XmlNode Child2 = Child1;
+                tll = 0;
+                foreach (XmlNode a in Child1)
+                {
+                    if (a.Name == "body")
+                        break;
+                    tll++;
+                }
+                XmlNode Child3 = Child1.ChildNodes.Item(tll);
+                XmlNode Child4 = Child3;
+                for (int i = 0; i < Convert.ToInt64(nodes.Count.ToString()); i++)
+                {
+                    if (worker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                    }
+                    else
+                    {
+                        Child3.ReplaceChild(nodes[i], nodes2[i]);
+                        worker.ReportProgress(i);
+                    }
+                }
+                Child1.ReplaceChild(Child3, Child4);
+                Child0.ReplaceChild(Child1, Child2);
+                xliff.ReplaceChild(Child0, xliff.GetElementsByTagName("xliff")[0]);
+                xliff.Save(saveloc);
+            }
+            return 0;
+        }
+
+    }
+}
